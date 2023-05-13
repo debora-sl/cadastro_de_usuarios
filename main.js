89bc95f764261b0c3f46c1f6c4c80a5e25a1d237
debora-sl <debora.souzalima22@gmail.com>
2023-05-12 23:00:10 -0300

    Finalizando validação e salvando usuarios no localStorage, acessando usuarios no localStorage, incluindo função que redireciona usuario para página inicial de usuario logado

--- main.js
+++ main.js
@@ -9,16 +9,14 @@ formularioCadastro.addEventListener("submit", (evento)=>{
     evento.preventDefault();
 
     //pegando os inputs de usuario e senha
-    const nome = evento.target.elements['nome'];
-    const senha = evento.target.elements['senha'];
+    var nome = evento.target.elements['nome'];
+    var senha = evento.target.elements['senha'];
 
 
     //chamando função que cria um usuário
     criaUsuario(nome.value, senha.value);
 
-    //limpando os inputs
-    nome.value = "";
-    senha.value = "";
+    limpaCampos(nome, senha);
 
 });
 
@@ -28,6 +26,7 @@ function criaUsuario(nome, senha) {
     //criando os novos usuarios
     const novoNome = nome;
     const novaSenha = senha;
+    var usuarioCadastros = [];
 
     //criando objeto usuario
     const usuario = {
@@ -38,12 +37,31 @@ function criaUsuario(nome, senha) {
     //incluindo o objeto dentro do array
     usuarios.push(usuario);
 
-    //salvando os itens no localStorage
+    //salvando os usuarios no localStorage
     localStorage.setItem("usuario", JSON.stringify(usuarios));
+
     alert(`Usuario ${novoNome}, cadastrado com sucesso!`);
+
+    setTimeout(function() {
+        window.location.assign("file:///C:/Users/debor/OneDrive/%C3%81rea%20de%20Trabalho/conquiste_sua_vaga_projetos/salao_de_cabeleleiro_cadastro_de_usuario/usuario_logado.html");
+    }, 3000);
+   
+    //pegando os usuarios no localStorage
+    usuarioCadastros = localStorage.getItem("usuario");
+    usuarioCadastros = JSON.parse(usuarioCadastros);
+    console.log(usuarioCadastros);
+
+
+
 };
 
-//Login
+function limpaCampos(nome, senha) {
+    //limpando os inputs
+    nome.value = "";
+    senha.value = "";       
+    }
+
+/*Login
 formularioLogin.addEventListener("submit", (evento)=>{
     evento.preventDefault();
 
@@ -51,4 +69,4 @@ formularioLogin.addEventListener("submit", (evento)=>{
 
 
 
-});
+}); */
